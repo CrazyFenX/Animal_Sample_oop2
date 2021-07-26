e8a02333577e001316fea8ad5068d0bc47a1b8ef
Кирилл <kattoshkin@gmail.com>
2021-07-26 13:15:07 +0700

    Добавьте файлы проекта.

---
+++ Animal_Sample_oop2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Animal_Sample_oop2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Animal cat = new Animal("Pushok", 2.3f, 'b');
+            ////cat.color = 'w';
+            ////cat.name = "pushok";
+            ////cat.mass = 2.3f;
+            //cat.say();
+
+            Cat cat = new Cat("Pushok", 2.3f, 'w', 0.4f);
+            Dog dog = new Dog("Sharik", 5.3f, 'g');
+
+            cat.say();
+            Console.WriteLine();
+            dog.say();
+        }
+    }
+}
